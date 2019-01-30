34706deaf38d2dc7a98375006d6e84b03cb77cfc
1046927100 <1046927100@qq.com>
2019-01-30 21:47:06 +0800

    111

--- foodend4/js/index.js
+++ foodend4/js/index.js
@@ -469,77 +469,115 @@
 				
 			},5000)
 	
-//	//分享给微信好友
-//	var imgUrl = 'img/pic300.jpg';  // 分享后展示的一张图片
-//	var lineLink = 'https://1046927100.github.io/pero/foodend4/index.html'; // 点击分享后跳转的页面地址
-//	var descContent = "小时候，我们以为永远也不会离开家，每天都可以吃到妈妈做的菜。长大后我们背井离乡...";  // 分享后的描述信息
-//	var shareTitle = '家乡的味道';  // 分享后的标题
-//	
-//	var appid = '';  // 应用id,如果有可以填，没有就留空
-//	
-//	        function shareFriend() {
-//	
-//	            WeixinJSBridge.invoke('sendAppMessage',{
-//	
-//	                "appid": appid,
-//	
-//	                "img_url": imgUrl,
-//	
-//	                "link": lineLink,
-//	
-//	                "desc": descContent,
-//	
-//	                "title": shareTitle
-//	
-//	            }, function(res) {
-//	
-//	          //_report('send_msg', res.err_msg);  // 这是回调函数，必须注释掉
-//	
-//	            })
-//	
-//	        }
-//	
-//	        function shareTimeline() {
-//	
-//	            WeixinJSBridge.invoke('shareTimeline',{
-//	
-//	                "img_url": imgUrl,
-//	
-//	                "link": lineLink,
-//	
-//	                "desc": descContent,
-//	
-//	                "title": shareTitle
-//	
-//	            }, function(res) {
-//	
-//	         //_report('timeline', res.err_msg); // 这是回调函数，必须注释掉
-//	
-//	            });
-//	
-//	        }
-	
-	   // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
-		
-		onBridgeReady = function(){
-			bgmusic.play();
-	            // 发送给好友
-	
-//	            WeixinJSBridge.on('menu:share:appmessage', function(){
-//	
-//	                shareFriend();
-//	
-//	            });
-//	
-//	            // 分享到朋友圈
-//	
-//	            WeixinJSBridge.on('menu:share:timeline', function(){
-//	
-//	                shareTimeline();
-//	
-//	            });
-			}
-	   document.addEventListener('WeixinJSBridgeReady',onBridgeReady, false);
 
-		
+	//微信分享
+	var imgUrl = 'img/pic300.jpg';  // 分享后展示的一张图片
+   var lineLink = 'http://hrjbt6.natappfree.cc/'; // 点击分享后跳转的页面地址
+   var descContent = "小时候，我们以为永远也不会离开家，每天都可以吃到妈妈做的菜。长大后...";  // 分享后的描述信息
+   var shareTitle = '家乡的味道';  // 分享后的标题
+
+var appid = '';  // 应用id,如果有可以填，没有就留空
+
+        function shareFriend() {
+
+            WeixinJSBridge.invoke('sendAppMessage',{
+
+                "appid": appid,
+
+                "img_url": imgUrl,
+
+                "img_width": "200",
+
+                "img_height": "200",
+
+                "link": lineLink,
+
+                "desc": descContent,
+
+                "title": shareTitle
+
+            }, function(res) {
+
+          //_report('send_msg', res.err_msg);  // 这是回调函数，必须注释掉
+
+            })
+
+        }
+
+        function shareTimeline() {
+
+            WeixinJSBridge.invoke('shareTimeline',{
+
+                "img_url": imgUrl,
+
+                "img_width": "200",
+
+                "img_height": "200",
+
+                "link": lineLink,
+
+                "desc": descContent,
+
+                "title": shareTitle
+
+            }, function(res) {
+
+         //_report('timeline', res.err_msg); // 这是回调函数，必须注释掉
+
+            });
+
+        }
+
+        function shareWeibo() {
+
+            WeixinJSBridge.invoke('shareWeibo',{
+
+                "content": descContent,
+
+                "url": lineLink,
+
+            }, function(res) {
+
+                //_report('weibo', res.err_msg);
+
+            });
+
+        }
+
+   // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+
+   document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+
+            // 发送给好友
+
+            WeixinJSBridge.on('menu:share:appmessage', function(argv){
+
+                shareFriend();
+
+            });
+
+            // 分享到朋友圈
+
+            WeixinJSBridge.on('menu:share:timeline', function(argv){
+
+                shareTimeline();
+
+            });
+
+            // 分享到微博
+
+            WeixinJSBridge.on('menu:share:weibo', function(argv){
+
+                shareWeibo();
+
+            });
+
+        }, false);
+	
+	
+	
+	
+	
+	
+	
 	}
