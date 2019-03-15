5007bffb777998ddb408b9968d8d8eae8284d3a4
1046927100 <1046927100@qq.com>
2019-03-15 16:01:47 +0800

    111

---
+++ shiping/js/new_file.js
@@ -0,0 +1,70 @@
+//	适配移动端页面大小的方法
+	var a = document.documentElement.style.fontSize = document.documentElement.clientWidth / 10 + 'px';
+	var scale = 1 / devicePixelRatio;
+	document.querySelector('meta[name="viewport"]').setAttribute('content','initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+	(function flexible (window, document) {
+	  var docEl = document.documentElement
+	  var dpr = window.devicePixelRatio || 1
+	  // adjust body font size
+	  function setBodyFontSize () {
+	    if (document.body) {
+	      document.body.style.fontSize = (12 * dpr) + 'px'
+	    }
+	    else {
+	      document.addEventListener('DOMContentLoaded', setBodyFontSize)
+	    }
+	  }
+	  setBodyFontSize();
+	  // set 1rem = viewWidth / 10
+	  function setRemUnit () {
+	    var rem = docEl.clientWidth / 10
+	    docEl.style.fontSize = rem + 'px'
+	  }
+	  setRemUnit()
+	  // reset rem unit on page resize
+	  window.addEventListener('resize', setRemUnit)
+	  window.addEventListener('pageshow', function (e) {
+	    if (e.persisted) {
+	      setRemUnit()
+	    }
+	  })
+	
+	  // detect 0.5px supports
+	  if (dpr >= 2) {
+	    var fakeBody = document.createElement('body')
+	    var testElement = document.createElement('div')
+	    testElement.style.border = '.5px solid transparent'
+	    fakeBody.appendChild(testElement)
+	    docEl.appendChild(fakeBody)
+	    if (testElement.offsetHeight === 1) {
+	      docEl.classList.add('hairlines')
+	    }
+	    docEl.removeChild(fakeBody)
+	  }
+	}(window, document));
+	
+	//监听视频是否播放
+	var vdo = document.getElementsByTagName("video")[0];
+	$(".shiping").on('timeupdate',function(){
+		$(".start").hide();
+	});
+	//点击播放
+	$(".start").on("click",function(){
+		vdo.play();
+	})
+	//微信自动播放
+	$(function(){
+		vdo.play();
+			function audioAutoPlay(){
+				play =function(){
+					vdo.play();
+					window.removeEventListener("touchstart",play,false);
+				};
+				window.addEventListener('YixinJSBridgeReady',function() {
+					vdo.play();
+				},false);
+				window.addEventListener("touchstart",play,false);
+				window.addEventListener('WeixinJSBridgeReady',play,false);
+			}
+			audioAutoPlay();
+	})
