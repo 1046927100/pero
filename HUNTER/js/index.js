d7553c5af5fadca3970b7afc18a62730ac7a3e6d
1046927100 <1046927100@qq.com>
2019-04-25 15:22:31 +0800

    33

--- HUNTER/js/index.js
+++ HUNTER/js/index.js
@@ -123,10 +123,11 @@ if (browser.versions.mobile) {//判断是否是移动设备打开。browser代�
         }
         if (browser.versions.ios) {
                 //是否在IOS浏览器打开
+                $(".shengyin").css("display","none")
         } 
         if (browser.versions.android){
                 //是否在安卓浏览器打开
-//              $(".shengyin").css("display","block")
+                
         } 
 		} else {
 		        //否则就是PC浏览器打开
